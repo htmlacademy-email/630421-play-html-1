562ba5b59e879a5eeceacdf963db14c3cf3b309f
ysveshnikov <ysveshnikov@ivi.ru>
2021-03-29 22:27:39 +0300

    Оптимизирует генерацию писем

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-const gulp = require('gulp');
+const { src, dest } = require('gulp');
 const inlineCss = require('gulp-inline-css');
 
+const INDEX_EMAIL = 2;
+
+const sources = [
+  '1-invitation',
+  '2-welcome',
+  '3-promo'
+];
+
+
 const styles = () => {
-  return gulp.src('1-invitation/*.html') // Путь, где лежат html-файлы, которые нужно инлайнить
+  return src(`${sources[INDEX_EMAIL]}/*.html`)
     .pipe(inlineCss({
       removeHtmlSelectors: true,
-    })) // Вызов функции
-    .pipe(gulp.dest('build/1-invitation/')); // Путь, куда будет положен результат
+    }))
+    .pipe(dest(`build/${sources[INDEX_EMAIL]}`)); // Путь, куда будет положен результат
 };
 
 exports.default = styles;
